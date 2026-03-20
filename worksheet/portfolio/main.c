e4460f1a383cbf2cdb127d267aac23e24eb5a917
s-flegg <161213652+s-flegg@users.noreply.github.com>
2026-03-20 16:29:54 +0000

    portfolio task

--- worksheet/portfolio/main.c
+++ worksheet/portfolio/main.c
@@ -6,9 +6,34 @@
 
 int main( void ) {
 
-    Point p1 = makePoint(0.0,0.0);
+    // Point p1 = makePoint(0.0,0.0);
     
-    // test your code by calling the functions and printing the output 
+    // // test your code by calling the functions and printing the output 
+    // Point p2 = makePoint(1, 1);
+    // Point p3 = makePoint(2, 1);
+    // Line l = makeLine(p2, p3);
+    // printf("%f\n", lineLength(l));
     
+    // Point p4 = makePoint(1, 0);
+    // Point p5 = makePoint(0, 1);
+    // Triangle t = makeTriangle(p1, p4, p5);
+    // printf("%f\n", triangleArea(t));
+
+    // printf("%d\n", pointInTriangle(p2, t));
+
+    // Point p6 = makePoint(2, 2);
+    // Point p7 = makePoint(3, 2);
+    // Line l2 = makeLine(p6, p7);
+    // printf("%d\n", pointInLine(p6, l2));
+
+    Point p8 = makePoint(0, 3);
+    Point p9 = makePoint(4, 0);
+    Line l3 = makeLine(p8, p9);
+    printf("line length main: %f\n", lineLength(l3));
+    Point p10 = makePoint(0, 2.99999);
+    printf("point in line main: %d\n", pointInLine(p10, l3));
+
+    // problem with same point?
+    printf("same point main: %d\n", samePoint(p8, p10));
     return 0;
 }
--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -4,9 +4,12 @@
 
 #include "shapes.h"
 
+#include <stdlib.h>
+
 Point makePoint( float x, float y ) {
     Point new;
-    // implementation
+    new.x = x;
+    new.y = y;
     return new;
 }
 
@@ -14,3 +17,54 @@ Point makePoint( float x, float y ) {
 // - start with stubs as above
 // - compile regularly to test syntax
 // - test functions by calling them from main()
+
+Line makeLine( Point p1, Point p2) {
+    Line new;
+    new.p[0] = p1;
+    new.p[1] = p2;
+    return new;
+}
+
+Triangle makeTriangle( Point p1, Point p2, Point p3) {
+    Triangle new;
+    new.p[0] = p1;
+    new.p[1] = p2;
+    new.p[2] = p3;
+    return new;
+}
+
+float lineLength( Line l) {
+    float cx = fabs(l.p[0].x - l.p[1].x);
+    float cy = fabs(l.p[0].y - l.p[1].y);
+
+    float length = sqrt(pow(cx, 2) + pow(cy, 2));
+    return length;
+}
+float triangleArea( Triangle t) {
+    // A = sqrt(s(s-a)(s-b)(s-c))
+    // s = 0.5(a+b+c)
+
+    // make sides
+    float a = lineLength(makeLine(t.p[0], t.p[1]));
+    float b = lineLength(makeLine(t.p[1], t.p[2]));
+    float c = lineLength(makeLine(t.p[2], t.p[0]));
+    
+    // semiperimiter
+    float s = 0.5 * (a + b + c);
+
+    // area
+    return (float) sqrt(s * (s - a) * (s - b) * (s - c));
+}
+
+// return true only if the point is the same location
+bool samePoint( Point p1, Point p2) {
+    return (fabs(lineLength(makeLine(p1,p2)))<1.0e-6) ? true: false;
+}
+// return true only if the point is one of the line endpoints
+bool pointInLine( Point p, Line l) {
+    return samePoint(p, l.p[0]) || samePoint(p, l.p[1]);
+}
+// return true only if the point is one of the triangle vertices
+bool pointInTriangle( Point p, Triangle t) {
+    return samePoint(p, t.p[0]) || samePoint(p, t.p[1]) || samePoint(p, t.p[2]);
+}
